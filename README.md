b7d4217739a26a08461c3e0e89a282dce065966d
Matt Nickerson <mattwnickerson39@gmail.com>
2025-11-20 20:08:29 -0500

    final

--- README.md
+++ README.md
@@ -4,5 +4,36 @@ Student: Matthew Nickerson
 Project: Project 2
 Class: CS3516
 
+## How to Run the Server
+
+1. Open a terminal or command prompt in the project directory.
+
+2. **Run the proxy server** with the following command:
+   ```
+   python "proxy server_skeleton code.py" <SERVER_IP>
+   ```
+   
+   Replace `<SERVER_IP>` with the IP address where the proxy should listen. Common options:
+   - `127.0.0.1` – Listen on localhost only (for local testing)
+   
+   You should see the output: `Ready to serve...`
+
+### Testing the Proxy Server
+
+Once the server is running, you can test it using a web browser or command-line tools:
+
+#### Option 1: Using a Browser
+1. Use the proxy at `http://<SERVER_IP>:8888`
+2. Go to a web page (e.g., `http://<SERVER_IP>:8888/baidu.com`)
+3. The proxy will fetch the page and cache it locally
+4. Clear browser data and search again once cached, terminal will display `Read from cache`
+
+### Server Details
+
+- **Port:** 8888 (fixed in the code)
+- **Protocol:** HTTP/1.0
+- **Caching:** The proxy caches requested pages locally in the current directory
+
+
 
 
---
+++ proxy server_skeleton code.py
@@ -0,0 +1,123 @@
+from socket import *
+import sys
+
+if len(sys.argv) <= 1:
+	print('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
+	sys.exit(2)
+	
+# Create a server TCP socket 
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+# Bind the socket to a port and start listening
+#Fill in start
+tcpSerSock.bind((sys.argv[1], 8888))
+tcpSerSock.listen(1)
+#Fill in end
+
+while 1:
+	# Strat receiving data from the client
+	print('Ready to serve...')
+	tcpCliSock, addr = tcpSerSock.accept()
+	print('Received a connection from:', addr)
+	message = tcpCliSock.recv(1024).decode()
+	print('xy:message', message, '\r\n')
+	
+	if not message or len(message.split()) < 2:
+		print("Invalid HTTP request received.")
+		tcpCliSock.close()
+		continue
+	
+	# Extract the filename from the given message
+	print('xy:message_split', message.split()[1], '\r\n')
+	
+	try:
+		filename = message.split()[1].partition("/")[2]
+	except IndexError:
+		print("Malformed request. Closing connection.")
+		tcpCliSock.close()
+		continue
+
+	print('xy:filename', filename)
+	
+	fileExist = "false"
+	filetouse = "/" + filename
+	print('xy:filetouse', filetouse, '\r\n')
+
+	try:
+		# Check wether the file exist in the cache
+		f = open(filetouse[1:], "rb")                      
+		outputdata = f.readlines()                        
+		fileExist = "true"
+		# ProxyServer finds a cache hit and needs to generate a response message
+		#Fill in start
+		tcpCliSock.send("HTTP/1.0 200 OK\r\n".encode())
+		#Fill in end
+		tcpCliSock.send("Content-Type:text/html\r\n".encode())
+		
+		# Socket then needs to send the file data to the original client
+		#Fill in start
+		for line in outputdata:
+			tcpCliSock.send(line)
+		#Fill in end
+		print('Read from cache')     
+			
+	# Error handling for file not found in cache
+	except IOError:
+		if fileExist == "false": 
+			# Create a socket on the proxyserver to connect with original server
+			c = socket(AF_INET, SOCK_STREAM)
+   			#Fill in start      #Fill in end         
+			if "/" in filename:
+				hostn, path = filename.split("/", 1)
+				path = "/" + path
+			else:
+				hostn = filename
+				path = "/"
+
+			print('xy:hostn', hostn, '\r\n')
+			print('xy:path', path, '\r\n')                                   
+			try:
+				# Connect to the socket to port 80
+				#Fill in start
+				c.connect((hostn, 80))
+				#Fill in end
+				# Create a temporary file on this socket so that later the socket can be used to read data
+				fileobj = c.makefile('rb', buffering=0)    
+				#send a request to original server to get the file requested by original client
+				#Fill in start
+				request_line = "GET " + path + " HTTP/1.0\r\nHost: www." + hostn + "\r\n\r\n"
+				c.send(request_line.encode())
+				#Fill in end          
+  
+				# Read the response into buffer
+				print('xy:begin to read response into buffer \r\n')                                   
+				buff = fileobj.readlines()
+				# Create a new file in the cache for the requested file. 
+				tmpFile = open("./" + hostn,"wb")  
+				#Also send the response in the buffer to client socket and the corresponding file in the cache
+				#Fill in start
+				tcpCliSock.send("HTTP/1.0 200 OK\r\n".encode())
+				for line in buff:
+					tmpFile.write(line)
+					tcpCliSock.send(line)
+				#Fill in end
+			except:
+				print("Illegal request: requested URL is probably wrong and 404 message will be sent")  
+				#if the requested webpage does not exist, send back a 404 response message to the original client  
+				#Fill in start
+				tcpCliSock.send("HTTP/1.0 404 Not Found\r\n".encode())
+				#Fill in end                           
+				tcpCliSock.send("Content-Type:text/html\r\n".encode())
+				tcpCliSock.send("\r\n".encode())                                           
+		else:
+			# HTTP response message for file not found
+			#Fill in start
+			tcpCliSock.send("HTTP/1.0 404 Not Found\r\n".encode())
+			#Fill in end  
+			tcpCliSock.send("Content-Type:text/html\r\n".encode())
+			tcpCliSock.send("\r\n".encode())
+	# Close the connection socket connected to the original client
+	#Fill in start
+	tcpCliSock.close()
+	#Fill in end  
+# Close the welcoming server socket   
+tcpSerSock.close()
